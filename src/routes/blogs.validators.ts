1c45e2fa6e1e4a729092ed1c4015a27b8b81c81f
nicorum <nicorum@yandex.ru>
2025-03-09 00:50:17 +0500

    fixed trimming and validation order

--- src/routes/blogs.validators.ts
+++ src/routes/blogs.validators.ts
@@ -1,20 +1,21 @@
 import { body } from "express-validator";
 
 const blogNameValidator = body('name')
-  .notEmpty().withMessage('Name should not be empty')
   .isString().withMessage('Name should be string')
   .trim()
+  .notEmpty().withMessage('Name should not be empty')
   .isLength({ max: 15 }).withMessage('Name should not be longer than 15 symbols')
 
 const blogDescrValidator = body('description')
-  .notEmpty().withMessage('Description should not be empty')
   .isString().withMessage('Description should be string')
   .trim()
+  .notEmpty().withMessage('Description should not be empty')
   .isLength({ max: 500 }).withMessage('Description should be 500 characters max')
 
 const blogWebsiteUrlValidator = body('websiteUrl')
-  .notEmpty().withMessage('Url should not be empty')
   .isString().withMessage('Website url should be string')
+  .trim()
+  .notEmpty().withMessage('Url should not be empty')
   .isLength({ max: 100 }).withMessage('Url should be 100 characters max')
   .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
   .withMessage('Website url should be a valid url');
--- src/routes/posts.validators.ts
+++ src/routes/posts.validators.ts
@@ -2,28 +2,28 @@ import { body } from "express-validator";
 import { blogRepository } from "../repositories/blogs.repository";
 
 const postTitleValidator = body('title')
-  .notEmpty().withMessage('Title should not be empty')
   .isString().withMessage('Title should be string')
   .trim()
+  .notEmpty().withMessage('Title should not be empty')
   .isLength({ max: 30 }).withMessage('Title should not be longer than 30 symbols')
 
 const postDescrValidator = body('shortDescription')
-  .notEmpty().withMessage('Description should not be empty')
   .isString().withMessage('Description should be string')
   .trim()
+  .notEmpty().withMessage('Description should not be empty')
   .isLength({ max: 100 }).withMessage('Description should be 100 characters max')
 
 const postContentValidator = body('content')
-  .notEmpty().withMessage('Body should not be empty')
   .isString().withMessage('Content should be string')
   .trim()
+  .notEmpty().withMessage('Body should not be empty')
   .isLength({ max: 1000 })
   .withMessage('Content should be 1000 characters max');
 
 const postBlogIdValidator = body('blogId')
-  .notEmpty().withMessage('BlogId should not be empty')
   .isString().withMessage('BlogId should be string')
   .trim()
+  .notEmpty().withMessage('BlogId should not be empty')
   .custom(async (blogId: string) => {
     const target = await blogRepository.findBlog(blogId);
     if (!target) {
