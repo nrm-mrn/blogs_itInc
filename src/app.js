0a52e8fa440899331b9f1b2f7fcbd5b5f46338e6
nicorum <nicorum@yandex.ru>
2025-03-08 23:14:56 +0500

    deleted compiled files from src

--- src/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-const settings_1 = require("./settings/settings");
-_1.app.listen(settings_1.SETTINGS.PORT, () => {
-    console.log(`Server started on port` + settings_1.SETTINGS.PORT);
-});
--- src/repositories/users.repository.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAdmin = void 0;
-const db_1 = require("../db/db");
-const getAdmin = () => {
-    return db_1.db.users[0];
-};
-exports.getAdmin = getAdmin;
